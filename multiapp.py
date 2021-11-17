0967e1f65297a3fa1f6cd0ab0d21420c9cbd8d58
Qiusheng Wu <giswqs@gmail.com>
2021-11-16 21:51:05 -0500

    Reverted sidebar click issue #9

--- multiapp.py
+++ multiapp.py
@@ -59,8 +59,8 @@ class MultiApp:
         app_state["page"] = st.session_state.radio
         # st.write('after', app_state)
 
-        # st.experimental_set_query_params(**app_state)
-        st.experimental_set_query_params(**st.session_state.to_dict())
+        st.experimental_set_query_params(**app_state)
+        # st.experimental_set_query_params(**st.session_state.to_dict())
         functions[titles.index(title)]()
 
         st.sidebar.title("Contribute")
