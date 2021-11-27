1c837f7410aa303cf33e6f157b85daa646faaf23
Qiusheng Wu <giswqs@gmail.com>
2021-11-27 12:31:33 -0500

    Reverted GDAL

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
-sudo add-apt-repository ppa:ubuntugis/ppa && sudo apt-get update
-sudo apt-get update
-sudo apt-get install python3-dev
-sudo apt-get install gdal-bin
-sudo apt-get install libgdal-dev
-export CPLUS_INCLUDE_PATH=/usr/include/gdal
-export C_INCLUDE_PATH=/usr/include/gdal
-gdal-config --version
-pip install GDAL==$(gdal-config --version | awk -F'[.]' '{print $1"."$2}') localtileserver
+# sudo add-apt-repository ppa:ubuntugis/ppa && sudo apt-get update
+# sudo apt-get update
+# sudo apt-get install python3-dev
+# sudo apt-get install gdal-bin
+# sudo apt-get install libgdal-dev
+# export CPLUS_INCLUDE_PATH=/usr/include/gdal
+# export C_INCLUDE_PATH=/usr/include/gdal
+# gdal-config --version
+# pip install GDAL==$(gdal-config --version | awk -F'[.]' '{print $1"."$2}') localtileserver
 
 mkdir -p ~/.streamlit/
 echo "\
