b604c9d104e4a5c9cf11114d2a088cff32341e30
Qiusheng Wu <giswqs@gmail.com>
2021-11-27 11:51:37 -0500

    Install GDAL

--- setup.sh
+++ setup.sh
@@ -1,3 +1,11 @@
+sudo add-apt-repository ppa:ubuntugis/ppa && sudo apt-get update
+sudo apt-get update
+sudo apt-get install gdal-bin
+sudo apt-get install libgdal-dev
+export CPLUS_INCLUDE_PATH=/usr/include/gdal
+export C_INCLUDE_PATH=/usr/include/gdal
+pip install GDAL
+
 mkdir -p ~/.streamlit/
 echo "\
 [server]\n\
