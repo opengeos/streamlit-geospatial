b45c3ea887e63d38efe25479ecf503da2bd779f4
Qiusheng Wu <giswqs@gmail.com>
2022-06-03 14:03:59 -0400

    Added hurricane app #46

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from apps import (
     heatmap,
     home,
     housing,
+    hurricane,
     plotly_maps,
     raster,
     timelapse,
@@ -28,6 +29,7 @@ apps = MultiApp()
 
 apps.add_app("Home", home.app)
 apps.add_app("Create Timelapse", timelapse.app)
+apps.add_app("Hurricane Mapping", hurricane.app)
 apps.add_app("U.S. Real Estate Data", housing.app)
 apps.add_app("U.S. Census Data", census.app)
 apps.add_app("Visualize Raster Data", raster.app)
