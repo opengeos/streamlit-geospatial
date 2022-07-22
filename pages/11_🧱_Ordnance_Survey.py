a1450532762a60609167a67b32e21ba4981486f8
Qiusheng Wu <giswqs@gmail.com>
2022-07-22 15:13:38 -0400

    Updated split map

--- pages/11_🧱_Ordnance_Survey.py
+++ pages/11_🧱_Ordnance_Survey.py
@@ -28,12 +28,12 @@ basemaps = leafmap.basemaps
 names = df["Name"].values.tolist() + list(basemaps.keys())
 links = df["URL"].values.tolist() + list(basemaps.values())
 
-col1, col2, col3, col4, col5, col6 = st.columns([3, 3, 1, 1, 1, 1.5])
+col1, col2, col3, col4, col5, col6, col7 = st.columns([3, 3, 1, 1, 1, 1.5, 1.5])
 with col1:
     left_name = st.selectbox(
         "Select the left layer",
         names,
-        index=names.index("Great Britain - OS 1:25,000, 1937-61"),
+        index=names.index("Great Britain - Bartholomew Half Inch, 1897-1907"),
     )
 
 with col2:
@@ -57,6 +57,17 @@ with col5:
 
 with col6:
     checkbox = st.checkbox("Add OS 25 inch")
+
+# with col7:
+with st.expander("Acknowledgements"):
+    markdown = """
+    The map tile access is by kind arrangement of the National Library of Scotland on the understanding that re-use is for personal purposes. They host most of the map layers except these:
+- The Roy Maps are owned by the British Library.
+- The Great Britain – OS maps 1:25,000, 1937-61 and One Inch 7th series, 1955-61 are hosted by MapTiler.
+
+If you wish you use these layers within a website, or for a commercial or public purpose, please view the National Library of Scotland Historic Maps Subscription API or contact them at maps@nls.uk.
+    """
+    st.markdown(markdown, unsafe_allow_html=True)
 
 m = leafmap.Map(
     center=[float(lat), float(lon)],
