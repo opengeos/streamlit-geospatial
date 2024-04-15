ce0fcd3a2b27781a484ac285ea22827b8a070a0a
AmirSarrafzadeh <sarrafzadeharasi.1871766@studenti.uniroma1.it>
2024-04-15 08:45:01 +0200

    first commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,18 +3,12 @@ import leafmap.foliumap as leafmap
 
 st.set_page_config(layout="wide")
 
-st.sidebar.info(
-    """
-    - Web App URL: <https://streamlit.geemap.org>
-    - GitHub repository: <https://github.com/giswqs/streamlit-geospatial>
-    """
-)
 
 st.sidebar.title("Contact")
 st.sidebar.info(
     """
-    Qiusheng Wu: <https://wetlands.io>
-    [GitHub](https://github.com/giswqs) | [Twitter](https://twitter.com/giswqs) | [YouTube](https://www.youtube.com/c/QiushengWu) | [LinkedIn](https://www.linkedin.com/in/qiushengwu)
+    [GitHub](https://github.com/AmirSarrafzadeh) |
+    [LinkedIn](https://www.linkedin.com/in/amir-sarrafzadeh/)
     """
 )
 
@@ -23,18 +17,17 @@ st.title("Streamlit for Geospatial Applications")
 
 st.markdown(
     """
-    This multipage app template demonstrates various interactive web apps created using [streamlit](https://streamlit.io) and [leafmap](https://leafmap.org). It is an open-source project and you are very welcome to contribute to the [GitHub repository](https://github.com/giswqs/streamlit-multipage-template).
+    This multipage app template demonstrates various interactive web apps created using [streamlit](https://streamlit.io) and [leafmap](https://leafmap.org). It is an open-source project and you are very welcome to contribute to the [GitHub repository](https://github.com/AmirSarrafzadeh/Streamlit_geospatial).
     """
 )
 
 st.header("Instructions")
 
 markdown = """
-1. For the [GitHub repository](https://github.com/giswqs/streamlit-multipage-template) or [use it as a template](https://github.com/giswqs/streamlit-multipage-template/generate) for your own project.
+1. For the [GitHub repository](https://github.com/AmirSarrafzadeh/Streamlit_geospatial).
 2. Customize the sidebar by changing the sidebar text and logo in each Python files.
 3. Find your favorite emoji from https://emojipedia.org.
 4. Add a new app to the `pages/` directory with an emoji in the file name, e.g., `1_🚀_Chart.py`.
-
 """
 
 st.markdown(markdown)
