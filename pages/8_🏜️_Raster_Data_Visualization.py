29ef836ee9b0048a771cf736d33d3749fc5daa39
Qiusheng Wu <giswqs@gmail.com>
2024-07-19 09:56:12 -0400

    Update hyperlinks (#137)

--- pages/8_🏜️_Raster_Data_Visualization.py
+++ pages/8_🏜️_Raster_Data_Visualization.py
@@ -8,7 +8,7 @@ st.set_page_config(layout="wide")
 
 st.sidebar.info(
     """
-    - Web App URL: <https://streamlit.geemap.org>
+    - Web App URL: <https://streamlit.gishub.org>
     - GitHub repository: <https://github.com/giswqs/streamlit-geospatial>
     """
 )
